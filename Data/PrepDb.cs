f22742c415fef1b4fcf84fdd734a10a41694eca7
Noni <iogyrtdledevyhek@outlook.com>
2022-08-07 17:17:24 +0300

    Add data layer DB Preparation

---
+++ Data/PrepDb.cs
@@ -0,0 +1,35 @@
+﻿using PLATFORMSERVICE.Models;
+
+namespace PLATFORMSERVICE.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(WebApplication app)
+        {
+            using (var serviceScope = app.Services.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("--> Seeding Data...");
+
+                context.Platforms.AddRange(
+                    new Platform() { Name = "Dot Net", Publsher = "Microsoft", Cost = "Free" },
+                    new Platform() { Name = "SQL Server Express", Publsher = "Microsoft", Cost = "Free" },
+                    new Platform() { Name = "Kubernetes", Publsher = "Cloud Native Computing Foundation", Cost = "Free" }
+                    );
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("--> We already have data");
+            }
+      
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using PLATFORMSERVICE.Data;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,11 +12,19 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseInMemoryDatabase("InMem"));
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PlatformSerivce", Version = "v1" });
-});
+//builder.Services.AddSwaggerGen(c =>
+//{
+//    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PlatformSerivce", Version = "v1" });
+//});
+
+
 var app = builder.Build();
+
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -24,7 +33,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+ 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
@@ -33,5 +42,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+
+PrepDb.PrepPopulation(app);
 
 app.Run();
